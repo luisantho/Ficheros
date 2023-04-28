91c222ba00892717e3f4ab9bc5f63d78aba650b4
luis <anthonytoapanta15@gmail.com>
2023-04-28 17:53:44 +0200

    Dia 2 Practtica Ficheros

--- src/PracticaFicheros/Alumno.java
+++ src/PracticaFicheros/Alumno.java
@@ -4,22 +4,33 @@
  */
 package PracticaFicheros;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
  *
  * @author luibol
  */
-public class Alumno {
+public class Alumno implements Serializable {
 
     //Atributos
     private int carnet;
     private String nombre;
     private String apellidos;
-    private int nota1, nota2, nota3;
+    private double nota1, nota2, nota3;
 
     //Constructor
-    public Alumno(int carnet, String nombre, String apellidos, int nota1, int nota2, int nota3) {
+    public Alumno(int carnet, String nombre, String apellidos, double nota1, double nota2, double nota3) {
         this.carnet = carnet;
         this.nombre = nombre;
         this.apellidos = apellidos;
@@ -27,8 +38,9 @@ public class Alumno {
         this.nota2 = nota2;
         this.nota3 = nota3;
     }
+
     //Constructor vacio 
-    public Alumno(){
+    public Alumno() {
     }
 
     //Setters
@@ -44,15 +56,15 @@ public class Alumno {
         this.apellidos = apellidos;
     }
 
-    public void setNota1(int nota1) {
+    public void setNota1(double nota1) {
         this.nota1 = nota1;
     }
 
-    public void setNota2(int nota2) {
+    public void setNota2(double nota2) {
         this.nota2 = nota2;
     }
 
-    public void setNota3(int nota3) {
+    public void setNota3(double nota3) {
         this.nota3 = nota3;
     }
     //Getters
@@ -69,60 +81,104 @@ public class Alumno {
         return apellidos;
     }
 
-    public int getNota1() {
+    public double getNota1() {
         return nota1;
     }
 
-    public int getNota2() {
+    public double getNota2() {
         return nota2;
     }
 
-    public int getNota3() {
+    public double getNota3() {
         return nota3;
     }
-    
+
     //ToString
     @Override
     public String toString() {
         return "Alumno" + "carnet:" + carnet + ", nombre:" + nombre + ", apellidos:" + apellidos + ", nota1:" + nota1 + ", nota2:" + nota2 + ", nota3:" + nota3;
     }
+
+    //Metodo ponerDatos
+    public static void ponerDatos(ArrayList<Alumno> lista) {
+        Scanner tcl = new Scanner(System.in);
+        boolean erroneo;
+
+        do {
+            erroneo = true;
+
+            try {
+                System.out.println("*****Introducir datos*****");
+                System.out.println("");
+
+                System.out.println("Introduce tu numero de carnet:");
+                int carnet = tcl.nextInt();
+
+                System.out.println("Introduce tu nombre:");
+                String nombre = tcl.nextLine();
+
+                System.out.println("Introduce tus apellidos:");
+                String apellidos = tcl.nextLine();
+
+                System.out.println("Introduce tu nota 1:");
+                int nota1 = tcl.nextInt();
+
+                System.out.println("Introduce tu nota 2:");
+                int nota2 = tcl.nextInt();
+
+                System.out.println("Introduce tu nota 3:");
+                int nota3 = tcl.nextInt();
+
+                Alumno a = new Alumno(carnet, nombre, apellidos, nota1, nota2, nota3);
+                lista.add(a);
+                System.out.println("Alumno añadido con exito");
+
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+
+        } while (erroneo);
+    }
+
+    //Metodo llenar Archivo
+    public static void llenarArchivo(ArrayList<Alumno> lista) {
+
+        ObjectOutputStream ost;
+        try {
+            ost = new ObjectOutputStream(new BufferedOutputStream(new FileOutputStream("alumnos.dat")));
+            ost.writeObject(lista);
+            ost.close();
+
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+        } catch (EOFException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
     
-    //Metodos
-    public static void ponerDatos(){
-        Alumno a = new Alumno();
-        Scanner tcl = new Scanner (System.in);
-        System.out.println("*****Introducir datos*****");
-        System.out.println("");
+    //Metodo pasarALista
+    public static void pasarALista(ArrayList <Alumno> lista){
+        
+            ObjectInputStream ist;
         try{
-        System.out.println("Introduce tu numero de carnet:");
-        int carnet = tcl.nextInt();
-        a.setCarnet(carnet);
+            ist = new ObjectInputStream(new BufferedInputStream(new FileInputStream("alumnos.dat")));
+                Alumno alumno = (Alumno) ist.readObject();
+                lista.add(alumno);
         
-        System.out.println("Introduce tu nombre:");
-        String nombre = tcl.nextLine();
-        a.setNombre(nombre);
         
-        System.out.println("Introduce tus apellidos:");
-        String apellidos = tcl.nextLine();
-        a.setApellidos(apellidos);
         
-        System.out.println("Introduce tu nota 1:");
-        int nota1 = tcl.nextInt();
-        a.setNota1(nota1);
-        
-         System.out.println("Introduce tu nota 2:");
-        int nota2 = tcl.nextInt();
-        a.setNota2(nota2);
-        
-        System.out.println("Introduce tu nota 3:");
-        int nota3 = tcl.nextInt();
-        a.setNota3(nota3);
-            
+        }catch(FileNotFoundException e){
+            System.out.println(e.getMessage());
+        }catch(EOFException e){
+            System.out.println(e.getMessage());
         }catch(Exception e){
             System.out.println(e.getMessage());
         }
     
-    }
     
-
+    
+    }
 }
+
